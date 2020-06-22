e2ed789a6dacc3a186dd81d73411aa8e1a8cdf54
Ujjwal <us36335@gmail.com>
2020-06-22 12:20:56 +0530

    workers migration

---
+++ models/workers.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Worker = sequelize.define('Worker', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {});
+  Worker.associate = function(models) {
+    // associations can be defined here
+  };
+  return Worker;
+};

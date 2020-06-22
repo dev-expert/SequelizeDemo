d9f17493f909aaa45caff1ec91e4f9e84817fe27
Ujjwal <us36335@gmail.com>
2020-06-22 13:40:51 +0530

    extra code removed

--- migrations/20200622054615-create-workerqwe.js
+++ migrations/20200622054615-create-workerqwe.js
@@ -9,27 +9,11 @@ module.exports = {
         {
           type: Sequelize.STRING
         }),
-      // queryInterface.addColumn(
-      //   'tryout_sections',
-      //   'import_section_id',
-      //   {
-      //     type: Sequelize.STRING
-      //   }),
-      // queryInterface.addColumn(
-      //   'tryout_questions',
-      //   'import_question_id',
-      //   {
-      //     type: Sequelize.STRING
-      //   })
-       
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
     return Promise.all([
-      // queryInterface.removeColumn('tryouts', 'import_tryout_id'),
-      // queryInterface.removeColumn('tryout_sections', 'import_section_id'),
-      // queryInterface.removeColumn('tryout_questions', 'import_question_id')
     ]);
   }
 };
